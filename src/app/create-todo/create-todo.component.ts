4fda7973ac313211c84bafe2e74a753b589eb460
mundo316 <raymundo.garcia316@yahoo.com>
2020-01-19 22:49:51 -0600

    no

--- src/app/create-todo/create-todo.component.ts
+++ src/app/create-todo/create-todo.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import input from //wherever the input is
-
+import input from; //wherever the input is;
+import TodoService from '../services/todo.service' 
 @Component({
   selector: 'app-create-todo',
   templateUrl: './create-todo.component.html',
   styleUrls: ['./create-todo.component.css']
 })
 export class CreateTodoComponent implements OnInit {
+//private readonly TodoService;
+  constructor() { 
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    //this.
   }
 
 
